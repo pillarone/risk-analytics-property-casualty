3c3e684b53e64431478c1c5791afdd5fe316b842
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-01-26 13:51:53 +0000

    upgraded to 0.4.6

--- RiskAnalyticsPCGrailsPlugin.groovy
+++ RiskAnalyticsPCGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 class RiskAnalyticsPCGrailsPlugin {
     // the plugin version
-    def version = "0.4.5"
+    def version = "0.4.6"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [
-    		"riskAnalyticsCore": "0.4.5.2"
+            "riskAnalyticsCore": "0.4.6"
     ]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
@@ -23,7 +23,7 @@ class RiskAnalyticsPCGrailsPlugin {
     // URL to the plugin's documentation
     def documentation = "http://www.pillarone.org"
 
-    def doWithWebDescriptor = { xml ->
+    def doWithWebDescriptor = {xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before 
     }
 
@@ -31,21 +31,21 @@ class RiskAnalyticsPCGrailsPlugin {
         // TODO Implement runtime spring config (optional)
     }
 
-    def doWithDynamicMethods = { ctx ->
+    def doWithDynamicMethods = {ctx ->
         // TODO Implement registering dynamic methods to classes (optional)
     }
 
-    def doWithApplicationContext = { applicationContext ->
+    def doWithApplicationContext = {applicationContext ->
         // TODO Implement post initialization spring config (optional)
     }
 
-    def onChange = { event ->
+    def onChange = {event ->
         // TODO Implement code that is executed when any artefact that this plugin is
         // watching is modified and reloaded. The event contains: event.source,
         // event.application, event.manager, event.ctx, and event.plugin.
     }
 
-    def onConfigChange = { event ->
+    def onConfigChange = {event ->
         // TODO Implement code that is executed when the project configuration changes.
         // The event is the same as for 'onChange'.
     }
