2983975a3638f0d1c2e7fa07af8860a73055a8d5
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-01-29 15:24:38 +0000

    enable code coverage

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,2 +1,24 @@
 grails.plugin.repos.discovery.pillarone ="https://readplugins:readplugins@svn.intuitive-collaboration.com/GrailsPlugins/"
 grails.plugin.repos.resolveOrder = ['pillarone', 'default', 'core']
+
+coverage {
+    exclusions = [
+            'models/**',
+            '**/org/grails/tomcat/*',
+            '**/org/pillarone/riskanalytics/core/**',
+            '**/*Test*',
+            '**/com/ulcjava/**',
+            '**/com/energizedwork/grails/plugins/jodatime/**',
+            '**/jasper/**',
+            '**/com/canoo/**',
+            '**/*Ulc*',
+            '**/*ULC*',
+            '**/grails/util/**',
+            '**/org/codehaus/**',
+            '**/org/grails/**',
+            '**/de/andreasschmitt/**',
+            '**GrailsPlugin**',
+            '**Taglib**',
+            '**TagLib**'
+    ]
+}
