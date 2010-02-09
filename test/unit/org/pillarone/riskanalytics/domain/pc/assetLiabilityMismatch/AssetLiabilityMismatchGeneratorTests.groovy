23d858c3551885294cac67f25647f572cddb1c73
Stephan Hartmann <SHartmann@munichre.com>
2010-02-09 18:00:02 +0000

    Test case for AssetLiabilityModelGenerator added.

---
+++ test/unit/org/pillarone/riskanalytics/domain/pc/assetLiabilityMismatch/AssetLiabilityMismatchGeneratorTests.groovy
@@ -0,0 +1,55 @@
+package org.pillarone.riskanalytics.domain.pc.assetLiabilityMismatch
+
+import org.pillarone.riskanalytics.core.components.Component
+import org.pillarone.riskanalytics.core.components.PeriodStore
+import org.pillarone.riskanalytics.core.simulation.engine.PeriodScope
+import org.pillarone.riskanalytics.domain.utils.RandomDistributionFactory
+import org.pillarone.riskanalytics.domain.utils.DistributionType
+import org.pillarone.riskanalytics.domain.utils.DistributionModifierFactory
+import org.pillarone.riskanalytics.domain.utils.DistributionModifier
+import org.pillarone.riskanalytics.core.parameterization.ComboBoxTableMultiDimensionalParameter
+import org.pillarone.riskanalytics.domain.pc.generators.claims.PerilMarker
+
+/**
+ * @author shartmann (at) munichre (dot) com
+ */
+
+class AssetLiabilityMismatchGeneratorTests extends GroovyTestCase {
+
+  void testUsageAbsoluteReserve() {
+    AssetLiabilityMismatchGenerator assetLiabilityMismatchGenerator = new AssetLiabilityMismatchGenerator(
+            parmDistribution: RandomDistributionFactory.getDistribution(DistributionType.CONSTANT,
+                    ["constant": 1.1d]),
+            parmModification: DistributionModifierFactory.getModifier(DistributionModifier.NONE, new HashMap()),
+            parmInitialVolume: 110d,
+            parmAssetLiabilityMismatchModel: AssetLiabilityMismatchGeneratorStrategyType.getStrategy(
+                    AssetLiabilityMismatchGeneratorStrategyType.ABSOLUTE, Collections.emptyMap())
+    )
+
+    assetLiabilityMismatchGenerator.periodScope = new PeriodScope();
+    assetLiabilityMismatchGenerator.periodScope.currentPeriod = 0
+    assetLiabilityMismatchGenerator.periodStore = new PeriodStore(assetLiabilityMismatchGenerator.periodScope)
+
+    assetLiabilityMismatchGenerator.doCalculation()
+    assertEquals 'initial volume', 110d, assetLiabilityMismatchGenerator.outInitialVolume.get(0).value
+    assertEquals '# packets, period 0', 1, assetLiabilityMismatchGenerator.outAlmResult.size()
+    assertEquals 'alm, period 0', 121d, assetLiabilityMismatchGenerator.outAlmResult[0].ultimate, 1e-8
+    
+    assetLiabilityMismatchGenerator.periodScope.currentPeriod++
+    assetLiabilityMismatchGenerator.reset()
+    assetLiabilityMismatchGenerator.doCalculation()
+    assetLiabilityMismatchGenerator.doCalculation()
+    assertEquals 'initial volume', 110d, assetLiabilityMismatchGenerator.outInitialVolume.get(0).value, 1e-8
+    assertEquals '# packets, period 1', 2, assetLiabilityMismatchGenerator.outAlmResult.size()
+    assertEquals 'alm, period 1', 242d, assetLiabilityMismatchGenerator.outAlmResult[0].ultimate +
+            assetLiabilityMismatchGenerator.outAlmResult[1].ultimate, 1e-8
+  }
+}
+
+class TestAlmGenerator extends Component implements PerilMarker {
+
+  protected void doCalculation() {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+}
