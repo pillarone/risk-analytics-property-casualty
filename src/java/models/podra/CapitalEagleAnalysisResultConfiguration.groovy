64b9831bedddb978724a9fed85cd70b93a679d3a
Jörg Dittrich <jdittrich@munichre.com>
2010-02-11 13:17:53 +0000

    New structured Paramerter Files for PODRA Model, includes reserve model, tested with RA0.4.9.

---
+++ src/java/models/podra/CapitalEagleAnalysisResultConfiguration.groovy
@@ -0,0 +1,68 @@
+package models.podra
+
+model = models.podra.PodraModel
+displayName = "CapitalEagle Analysis"
+components {
+	aggregateFinancials {
+		outAlm = "AGGREGATED"
+		outTotal = "AGGREGATED"
+		outUnderwriting = "AGGREGATED"
+	}
+	almGenerators {
+		outAlmResult = "AGGREGATED"
+		outInitialVolume = "AGGREGATED"
+		subsubcomponents {
+			outAlmResult = "AGGREGATED"
+			outInitialVolume = "AGGREGATED"
+		}
+	}
+	claimsGenerators {
+		outClaims = "AGGREGATED"
+		outClaimsLeanDevelopment = "AGGREGATED"
+		subsubcomponents {
+			outClaims = "AGGREGATED"
+			outClaimsLeanDevelopment = "AGGREGATED"
+		}
+	}
+	linesOfBusiness {
+		outClaimsDevelopmentLeanCeded = "AGGREGATED"
+		outClaimsDevelopmentLeanGross = "AGGREGATED"
+		outClaimsDevelopmentLeanNet = "AGGREGATED"
+		outUnderwritingInfoCeded = "AGGREGATED"
+		outUnderwritingInfoGross = "AGGREGATED"
+		outUnderwritingInfoNet = "AGGREGATED"
+		sublineOfBusiness {
+			outClaimsDevelopmentLeanCeded = "AGGREGATED"
+			outClaimsDevelopmentLeanGross = "AGGREGATED"
+			outClaimsDevelopmentLeanNet = "AGGREGATED"
+			outUnderwritingInfoCeded = "AGGREGATED"
+			outUnderwritingInfoGross = "AGGREGATED"
+			outUnderwritingInfoNet = "AGGREGATED"
+		}
+	}
+	reinsurance {
+		outClaimsDevelopmentLeanCeded = "AGGREGATED"
+		outClaimsDevelopmentLeanGross = "AGGREGATED"
+		outClaimsDevelopmentLeanNet = "AGGREGATED"
+		outCoverUnderwritingInfo = "AGGREGATED"
+		outNetAfterCoverUnderwritingInfo = "AGGREGATED"
+		outUnderwritingInfo = "AGGREGATED"
+		subcontracts {
+			outClaimsDevelopmentLeanCeded = "AGGREGATED"
+			outClaimsDevelopmentLeanGross = "AGGREGATED"
+			outClaimsDevelopmentLeanNet = "AGGREGATED"
+			outContractFinancials = "AGGREGATED"
+			outCoverUnderwritingInfo = "AGGREGATED"
+			outFilteredUnderwritingInfo = "AGGREGATED"
+			outNetAfterCoverUnderwritingInfo = "AGGREGATED"
+		}
+	}
+	reserveGenerators {
+		outClaimsLeanDevelopment = "AGGREGATED"
+		outInitialReserves = "AGGREGATED"
+		subsubcomponents {
+			outClaimsLeanDevelopment = "AGGREGATED"
+			outInitialReserves = "AGGREGATED"
+		}
+	}
+}
