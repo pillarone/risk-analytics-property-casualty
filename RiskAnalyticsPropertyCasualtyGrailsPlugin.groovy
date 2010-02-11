899f7f01a30bf9c28721010f933849774b903c69
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-02-11 14:50:14 +0000

    upgraded plugin version

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -8,13 +8,11 @@ import org.pillarone.riskanalytics.core.output.CollectingModeFactory
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
-    def version = "0.4.9.1"
+    def version = "0.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [
-            "riskAnalyticsCore": "0.4.9.1 > *"
-    ]
+    def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
