2f24f8267c5a0b09e69d341d72318788756b2b9c
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-02-20 20:35:02 +0000

    extends statement was missing

--- src/groovy/org/pillarone/riskanalytics/domain/pc/reinsurance/IReinsurerMarker.java
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/reinsurance/IReinsurerMarker.java
@@ -1,7 +1,9 @@
 package org.pillarone.riskanalytics.domain.pc.reinsurance;
 
+import org.pillarone.riskanalytics.core.components.IComponentMarker;
+
 /**
  * @author stefan.kunz (at) intuitive-collaboration (dot) com
  */
-public interface IReinsurerMarker {
+public interface IReinsurerMarker extends IComponentMarker {
 }
