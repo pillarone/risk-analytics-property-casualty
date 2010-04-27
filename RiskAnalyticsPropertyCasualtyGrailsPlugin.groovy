56df38f8acfabe656312b6c031708bc305e1ccb0
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-04-27 14:10:32 +0000

    releasing 0.6.1

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -10,7 +10,7 @@ import org.pillarone.riskanalytics.domain.pc.company.CompanyPortion
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
-    def version = "0.6.0.1"
+    def version = "0.6.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
