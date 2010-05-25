4fb8428ba556e3cb733e16a86238d57e879c7c1f
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-05-25 21:16:15 +0000

    fix build

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -10,8 +10,7 @@ import org.pillarone.riskanalytics.domain.pc.company.CompanyPortion
 import org.pillarone.riskanalytics.core.parameterization.validation.ValidatorRegistry
 import org.pillarone.riskanalytics.domain.utils.validation.DistributionTypeValidator
 import org.pillarone.riskanalytics.domain.pc.reinsurance.commissions.validation.CommissionStrategyTypeValidator
-import org.pillarone.riskanalytics.domain.pc.generators.claims.validation.ClaimsGeneratorStrategyValidator
-import org.pillarone.riskanalytics.domain.pc.generators.claims.validation.ClaimsGeneratorStrategyValidator
+//import org.pillarone.riskanalytics.domain.pc.generators.claims.validation.ClaimsGeneratorStrategyValidator
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
