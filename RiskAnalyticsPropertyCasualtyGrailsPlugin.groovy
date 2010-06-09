7ffc2752a2ee3d9746f5430525ed79a8d6241256
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-06-09 07:00:52 +0000

    releasing plugin version 1.0-ALPHA-3

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -14,7 +14,7 @@ import org.pillarone.riskanalytics.domain.pc.generators.claims.validation.Claims
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
-    def version = "1.0-ALPHA-2"
+    def version = "1.0-ALPHA-3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
