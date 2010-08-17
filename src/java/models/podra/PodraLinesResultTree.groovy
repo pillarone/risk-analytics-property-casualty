5680ed391f733d0d28380dbf0d0073e17f3ea236
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-08-17 07:32:07 +0000

    updated result tree files

--- src/java/models/podra/PodraLinesResultTree.groovy
+++ src/java/models/podra/PodraLinesResultTree.groovy
@@ -7,14 +7,14 @@ model = PodraModel
 displayName = "Lines of Business"
 
 mappings = [
-        "Podra:linesOfBusiness:outClaimsDevelopmentLeanGross:incurred":"Podra:claims:incurred",
-        "Podra:linesOfBusiness:subMotorHull:outClaimsDevelopmentLeanGross:incurred":"Podra:claims:incurred:motorHull",
-        "Podra:linesOfBusiness:subMotorThirdPartyLiability:outClaimsDevelopmentLeanGross:incurred":"Podra:claims:incurred:motorThirdPartyLiability",
-        "Podra:linesOfBusiness:subPersonalAccident:outClaimsDevelopmentLeanGross:incurred":"Podra:claims:incurred:personalAccident",
-        "Podra:linesOfBusiness:subProperty:outClaimsDevelopmentLeanGross:incurred":"Podra:claims:incurred:property",
-        "Podra:linesOfBusiness:outClaimsDevelopmentLeanGross:paid":"Podra:claims:paid",
-        "Podra:linesOfBusiness:subMotorHull:outClaimsDevelopmentLeanGross:paid":"Podra:claims:paid:motorHull",
-        "Podra:linesOfBusiness:subMotorThirdPartyLiability:outClaimsDevelopmentLeanGross:paid":"Podra:claims:paid:motorThirdPartyLiability",
-        "Podra:linesOfBusiness:subPersonalAccident:outClaimsDevelopmentLeanGross:paid":"Podra:claims:paid:personalAccident",
-        "Podra:linesOfBusiness:subProperty:outClaimsDevelopmentLeanGross:paid":"Podra:claims:paid:property",
+        "Podra:claims:incurred": "Podra:linesOfBusiness:outClaimsDevelopmentLeanGross:incurred",
+        "Podra:claims:incurred:motorHull": "Podra:linesOfBusiness:subMotorHull:outClaimsDevelopmentLeanGross:incurred",
+        "Podra:claims:incurred:motorThirdPartyLiability": "Podra:linesOfBusiness:subMotorThirdPartyLiability:outClaimsDevelopmentLeanGross:incurred",
+        "Podra:claims:incurred:personalAccident": "Podra:linesOfBusiness:subPersonalAccident:outClaimsDevelopmentLeanGross:incurred",
+        "Podra:claims:incurred:property": "Podra:linesOfBusiness:subProperty:outClaimsDevelopmentLeanGross:incurred",
+        "Podra:claims:paid": "Podra:linesOfBusiness:outClaimsDevelopmentLeanGross:paid",
+        "Podra:claims:paid:motorHull": "Podra:linesOfBusiness:subMotorHull:outClaimsDevelopmentLeanGross:paid",
+        "Podra:claims:paid:motorThirdPartyLiability": "Podra:linesOfBusiness:subMotorThirdPartyLiability:outClaimsDevelopmentLeanGross:paid",
+        "Podra:claims:paid:personalAccident": "Podra:linesOfBusiness:subPersonalAccident:outClaimsDevelopmentLeanGross:paid",
+        "Podra:claims:paid:property": "Podra:linesOfBusiness:subProperty:outClaimsDevelopmentLeanGross:paid",
 ]
