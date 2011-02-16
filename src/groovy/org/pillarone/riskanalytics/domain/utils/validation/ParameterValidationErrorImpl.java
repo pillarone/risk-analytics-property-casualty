efda91d8aa513dfe88685ba8f96b65268f312b6a
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-02-16 17:02:47 +0100

    PMO-1099: new core plugin is available

--- src/groovy/org/pillarone/riskanalytics/domain/utils/validation/ParameterValidationErrorImpl.java
+++ src/groovy/org/pillarone/riskanalytics/domain/utils/validation/ParameterValidationErrorImpl.java
@@ -2,22 +2,11 @@ package org.pillarone.riskanalytics.domain.utils.validation;
 
 import org.pillarone.riskanalytics.core.parameterization.validation.ParameterValidationError;
 
-import java.text.MessageFormat;
 import java.util.Collection;
-import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 
 public class ParameterValidationErrorImpl extends ParameterValidationError {
 
     public ParameterValidationErrorImpl(String message, Collection arguments) {
         super(message, arguments);
     }
-
-    // todo(sku): remove again once a new core plugin is available
-    @Override
-    public String getLocalizedMessage(Locale locale) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
 }
