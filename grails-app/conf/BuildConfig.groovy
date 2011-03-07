bb9cd97b372b5fa28a9d761080cdc8b29b88d2d4
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-03-07 08:38:18 +0100

    releasing plugin version 1.3-BETA-1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 
         test ":code-coverage:1.1.7"
 
-        runtime "org.pillarone:risk-analytics-core:1.3-ALPHA-2.3"
+        runtime "org.pillarone:risk-analytics-core:1.3-BETA-1"
     }
 }
 
