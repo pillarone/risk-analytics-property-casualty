bdde85e040e5b060aeea584c8a706d797f651c73
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-03-22 09:18:36 +0100

    build fix

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 
         test ":code-coverage:1.1.7"
 
-        if (appName == "risk-analytics-property-casualty") {
+        if (appName == "RiskAnalyticsPropertyCasualty") {
             runtime "org.pillarone:risk-analytics-core:1.3-BETA-2.3"
         }
     }
