224647b75a542b77b7cc157152002e2b095cdfea
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-05-27 15:18:58 +0200

    ART-549 fixed.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,7 +24,7 @@ grails.project.dependency.resolution = {
         test ":code-coverage:1.2.2"
 
         if (appName == "RiskAnalyticsPropertyCasualty") {
-            runtime "org.pillarone:risk-analytics-core:1.4-ALPHA-2.9.3"
+            runtime "org.pillarone:risk-analytics-core:1.4-ALPHA-3.2.1"
         }
     }
 }
