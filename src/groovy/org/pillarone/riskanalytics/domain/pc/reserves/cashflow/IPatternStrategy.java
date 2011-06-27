76cff768d9c5f50890e9f7c2f8cfeef2662bc9fe
unknown <sparten@.art-allianz.com>
2011-06-27 16:41:36 +0200

    Enable IPattern marker for new (stochastic) pattern behaviour

--- src/groovy/org/pillarone/riskanalytics/domain/pc/reserves/cashflow/IPatternStrategy.java
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/reserves/cashflow/IPatternStrategy.java
@@ -2,6 +2,7 @@ package org.pillarone.riskanalytics.domain.pc.reserves.cashflow;
 
 import org.joda.time.Period;
 import org.pillarone.riskanalytics.core.parameterization.IParameterObject;
+import org.pillarone.riskanalytics.core.parameterization.IParameterObjectClassifier;
 import org.pillarone.riskanalytics.domain.pc.constants.SimulationPeriod;
 
 import java.util.List;
@@ -15,4 +16,6 @@ public interface IPatternStrategy extends IParameterObject {
     List<Period> getCumulativePeriods();
     int patternLength();
     SimulationPeriod calibrationPeriod();
+    IParameterObjectClassifier getType();
+
 }
