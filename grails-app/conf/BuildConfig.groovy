130bed14fed529081019728bff8d31ec24b08bb9
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2011-08-09 11:59:30 +0200

    use new maven repo

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,7 +10,7 @@ grails.project.dependency.resolution = {
         grailsCentral()
     }
 
-    mavenRepo "https://build.intuitive-collaboration.com/maven/plugins/"
+    mavenRepo "https://repository.intuitive-collaboration.com/nexus/content/repositories/pillarone-public/"
 
     plugins {
         runtime ":background-thread:1.3"
@@ -33,18 +33,20 @@ grails.project.dependency.resolution = {
 //grails.plugin.location.'risk-analytics-commons' = "../risk-analytics-commons"
 
 grails.project.dependency.distribution = {
-    String passPhrase = ""
+    String password = ""
+    String user = ""
     String scpUrl = ""
     try {
         Properties properties = new Properties()
         properties.load(new File("${userHome}/deployInfo.properties").newInputStream())
 
-        passPhrase = properties.get("passPhrase")
+        user = properties.get("user")
+        password = properties.get("password")
         scpUrl = properties.get("url")
     } catch (Throwable t) {
     }
     remoteRepository(id: "pillarone", url: scpUrl) {
-        authentication username: 'root', privateKey: "${userHome.absolutePath}/.ssh/id_rsa", passphrase: passPhrase
+        authentication username: user, password: password
     }
 }
 
