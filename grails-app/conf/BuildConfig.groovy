d65ecd71a4fc7457cda9a231112ed6c8a6f65206
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-10-24 22:14:28 +0200

    releasing new plugin version 1.4-BETA-5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,7 +24,7 @@ grails.project.dependency.resolution = {
         test ":code-coverage:1.2.4"
 
         if (appName == "RiskAnalyticsPropertyCasualty") {
-            runtime "org.pillarone:risk-analytics-core:1.4-BETA-6"
+            runtime "org.pillarone:risk-analytics-core:1.4-BETA-9.2"
             runtime("org.pillarone:risk-analytics-commons:0.1.26") { transitive = false }
         }
     }
