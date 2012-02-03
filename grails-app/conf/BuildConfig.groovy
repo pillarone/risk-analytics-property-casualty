08eabd7cf1d507920abb699f2e9fbd6caf39ef1c
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-02-03 13:54:43 +0100

    branch specific plugins.dir set

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 //Use a custom plugins dir, because different branches use different plugin versions
-grails.project.plugins.dir = "../local-plugins/RiskAnalyticsPropertyCasualty-master"
+grails.project.plugins.dir = "../local-plugins/RiskAnalyticsPropertyCasualty-1.4.x"
 
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
