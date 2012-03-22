ff5e42e000bb042ab3e5f91206de0d7e8dd1ddc1
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2012-03-22 14:16:33 +0100

    added result navigator mapping to PodraModel

--- src/java/models/podra/PodraModel.groovy
+++ src/java/models/podra/PodraModel.groovy
@@ -83,4 +83,32 @@ class PodraModel extends StochasticModel {
     public String getDefaultResultConfiguration() {
         return "CapitalEagle Analysis"
     }
+
+    @Override
+    Closure createResultNavigatorMapping() {
+        return {
+            lob {
+                or {
+                    enclosedBy(prefix: ['linesOfBusiness:sub'], suffix: [':'])
+                    conditionedOn(value: 'Aggregate') {
+                        matching(toMatch: ["linesOfBusiness:(?!sub)"])
+                    }
+                }
+            }
+            peril {
+                enclosedBy(prefix: ["claimsGenerators:sub"], suffix: [":"])
+            }
+            reinsuranceContractType {
+                enclosedBy(prefix: ["subContracts:sub", "reinsuranceContracts:sub"], suffix: [":"])
+            }
+            accountBasis {
+                matching(toMatch: ["Gross", "Ceded", "Net"])
+            }
+            keyfigure {
+                synonymousTo(category: "Field")
+            }
+        }
+    }
+
+
 }
