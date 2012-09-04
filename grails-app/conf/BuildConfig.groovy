d030708a7138b224112511183ac10313ee1935b3
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-09-04 19:09:48 +0200

    updating plugin dependencies and releasing new plugin 1.6-ALPHA-4-kti

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,8 +25,8 @@ grails.project.dependency.resolution = {
         test ":excel-import:0.9.6"
 
         if (appName == "RiskAnalyticsPropertyCasualty") {
-            runtime "org.pillarone:risk-analytics-core:1.6-ALPHA-4.2-kti"
-            runtime("org.pillarone:risk-analytics-commons:0.4.6") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.6-ALPHA-5.7-kti"
+            runtime("org.pillarone:risk-analytics-commons:0.4.8") { transitive = false }
         }
     }
 }
