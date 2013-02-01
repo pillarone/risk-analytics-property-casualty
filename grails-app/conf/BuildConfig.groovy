d61d3ca6df6e8d8f4bac69497486a77b99b95b5d
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-02-01 09:36:12 +0100

    corrected plugin dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
         test ":excel-import:0.9.6"
 
         if (appName == "RiskAnalyticsPropertyCasualty") {
-            runtime "org.pillarone:risk-analytics-core:1.6-BETA-7-kti"
+            runtime "org.pillarone:risk-analytics-core:1.6-BETA-2.7-kti"
             runtime("org.pillarone:risk-analytics-commons:0.4.26") { transitive = false }
         }
     }
