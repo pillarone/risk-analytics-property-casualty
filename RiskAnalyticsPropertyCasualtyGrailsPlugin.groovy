a06977742f961840c19e70278e2a36e62b321a3f
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-02-23 21:19:25 +0100

    releasing new plugin version 1.6-RC-1-kti updating plugin dependencies

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import org.pillarone.riskanalytics.domain.utils.validation.DistributionTypeValid
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
-    def version = "1.6-BETA-3-kti"
+    def version = "1.6-RC-1-kti"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
