905a3ebe03f16e45ad7c605568fc8988980c3fd2
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-10-02 18:23:00 +0200

    releasing plugin version 1.8

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,8 +29,8 @@ grails.project.dependency.resolution = {
         compile ":excel-import:1.0.0"
 
         if (appName == "RiskAnalyticsPropertyCasualty") {
-            runtime "org.pillarone:risk-analytics-core:1.8-a1"
-            runtime("org.pillarone:risk-analytics-commons:1.8-a1") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.8"
+            runtime("org.pillarone:risk-analytics-commons:1.8") { transitive = false }
         }
     }
 
