3e4d584658c59104e119c6106ee48321c39710f3
fazl <fazl.rahman@art-allianz.com>
2014-06-06 16:50:53 +0200

    Propagate upgrade of core-19x to v1.9.1

--- RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
+++ RiskAnalyticsPropertyCasualtyGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import org.pillarone.riskanalytics.domain.utils.validation.DistributionTypeValid
 
 class RiskAnalyticsPropertyCasualtyGrailsPlugin {
     // the plugin version
-    def version = "1.9.1"
+    def version = "1.9.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3.2 > *"
     // the other plugins this plugin depends on
